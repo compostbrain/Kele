f3876c25cc4eae24f201034f521f50a09893442e
compostbrain <joel@wildmeadowsfarm.com>
2017-06-26 12:30:59 -0500

    Retrieve Users with get_me method

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,10 @@
 require 'httparty'
 require 'pp'
+require 'json'
 
 class Kele
   include HTTParty
+
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(user, password)
@@ -14,5 +16,11 @@ class Kele
     pp response.message
     @auth_token = response["auth_token"]
   end
+
+
+    def get_me
+        response = self.class.get("/users/me", headers: { authorization: @auth_token })
+        JSON.parse(response.body)
+    end
 
 end
