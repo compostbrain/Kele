11fedd334a0cd1f2f9b62676df830c3611b80437
compostbrain <joel@wildmeadowsfarm.com>
2017-07-09 12:03:25 -0400

    create_submission method added

--- lib/kele.rb
+++ lib/kele.rb
@@ -31,21 +31,36 @@ class Kele
 	end
 
   def get_messages(page)
-  response = self.class.get('/message_threads', headers: { authorization: @auth_token }, body: { page: page })
-  JSON.parse(response.body)
-end
+    response = self.class.get('/message_threads', headers: { authorization: @auth_token }, body: { page: page })
+    JSON.parse(response.body)
+  end
 
-def create_message(sender, recipient_id, subject, stripped_text)
-  body = {
-    sender: sender,
-    recipient_id: recipient_id,
+  def create_message(sender, recipient_id, subject, stripped_text)
+    body = {
+      sender: sender,
+      recipient_id: recipient_id,
 
-    subject: subject,
-    "stripped-text": stripped_text
-  }
+      subject: subject,
+      "stripped-text": stripped_text
+    }
 
-  response = self.class.post('/messages', headers: { authorization: @auth_token }, body: body)
+    response = self.class.post('/messages', headers: { authorization: @auth_token }, body: body)
 
-end
+  end
+
+  def create_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment)
+    body = {
+      assignment_branch: assignment_branch,
+      assignment_commit_link: assignment_commit_link,
+      checkpoint_id: checkpoint_id,
+      comment: comment
+    }
+
+
+    response = self.class.post(/checkpoint_submissions, headers: {authorization: @auth_token}, body: body)
+
+  end
+
+
 
 end
