aa75badba014c66c6b978e440913e98b49469d82
compostbrain <joel@wildmeadowsfarm.com>
2017-07-09 12:12:42 -0400

    updated create_submission method

--- lib/kele.rb
+++ lib/kele.rb
@@ -48,16 +48,17 @@ class Kele
 
   end
 
-  def create_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment)
+  def create_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment, enrollment_id)
     body = {
       assignment_branch: assignment_branch,
       assignment_commit_link: assignment_commit_link,
       checkpoint_id: checkpoint_id,
-      comment: comment
+      comment: comment,
+      enrollment_id: enrollment_id
     }
 
 
-    response = self.class.post(/checkpoint_submissions, headers: {authorization: @auth_token}, body: body)
+    response = self.class.post('/checkpoint_submissions', headers: {authorization: @auth_token}, body: body)
 
   end
 
